6a7379dff970826b042a69f86379b4570393ee95
Turbero <dicon18@gmail.com>
2024-10-24 17:40:40 +0200

    Removing unneeded option

--- DetailedLevels/ConfigurationFile.cs
+++ DetailedLevels/ConfigurationFile.cs
@@ -1,7 +1,6 @@
 ﻿using BepInEx.Configuration;
 using BepInEx;
 using UnityEngine;
-using System.Runtime.CompilerServices;
 
 namespace DetailedLevels
 {
@@ -13,7 +12,6 @@ namespace DetailedLevels
         public static ConfigEntry<int> numberOfDecimals;
         public static ConfigEntry<int> skillUpMessageAfterMultipleLevel;
         public static ConfigEntry<int> skillUpBigMessageAfterMultipleLevel;
-        public static ConfigEntry<bool> equipBuffs;
         public static ConfigEntry<Color> colorSkillBackground;
         //public static ConfigEntry<bool> birdsSkillIncrease;
 
@@ -30,7 +28,6 @@ namespace DetailedLevels
                 numberOfDecimals = config.Bind<int>("2 - Levels Data", "NumberOfDecimals", 2, "Number of decimals to show in your levels information (default = 2, min = 0, max = 15)");
                 skillUpMessageAfterMultipleLevel = config.Bind<int>("2 - Levels Data", "SkillUpMessageAfterMultipleLevel", 5, "Shows skill up message after the new level is multiple of the indicated level (0 = disabled, default = 5)");
                 skillUpBigMessageAfterMultipleLevel = config.Bind<int>("2 - Levels Data", "SkillUpBigMessageAfterMultipleLevel", 20, "Shows skill up big message after the new level is multiple of the indicated level (0 = disabled, default = 5)");
-                equipBuffs = config.Bind<bool>("2 - Levels Data", "equipBuffs", true, "Allows selecting buffs in the skills dialog to show them as buffs and look at how it is progressed");
                 colorSkillBackground = config.Bind<Color>("2 - Levels Data", "ColorSkillBackground", Color.cyan, "Choose the color background for selected skills in the skills dialog: red, green, blue, white, black, yellow, cyan, magenta, gray or grey (default = cyan)");
                 //birdsSkillIncrease = config.Bind<bool>("3 - Improvements", "birdsSkillIncrease", true, "Allows skilling up when killing birds and getting their feathers with the used weapon (default = true)");
             }
