be5649b9d1a1884eb358371d8c72e2a8c0312e62
Turbero <dicon18@gmail.com>
2025-10-02 13:43:23 +0200

    Skipping message if fighting a boss

--- DetailedLevels/Features/PlayerSkillupMessage.cs
+++ DetailedLevels/Features/PlayerSkillupMessage.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System;
+using System.Reflection;
 
 namespace DetailedLevels.Features
 {
@@ -10,17 +11,31 @@ namespace DetailedLevels.Features
         {
             //Check big message first
             int multipleBigValue = Math.Max(0, Math.Min(100, ConfigurationFile.skillUpBigMessageAfterMultipleLevel.Value));
-            if (multipleBigValue > 0 && ((int)level % multipleBigValue == 0 || (int)level == 100))
+            if (multipleBigValue > 0 && ((int)level % multipleBigValue == 0 || (int)level == 100) && !isFightingBoss())
             {
                 MessageHud.instance.ShowBiomeFoundMsg($"$skill_{skill.ToString().ToLower()}: {(int)level}", true);
                 return;
             }
 
+            //Check yellow message
             int multipleValue = Math.Max(0, Math.Min(100, ConfigurationFile.skillUpMessageAfterMultipleLevel.Value));
-            if (multipleValue > 0 && ((int)level % multipleValue == 0))
+            if (multipleValue > 0 && (int)level % multipleValue == 0 && !isFightingBoss())
             {
                 MessageHud.instance.ShowMessage(MessageHud.MessageType.Center, $"$msg_skillup $skill_{skill.ToString().ToLower()}: {(int)level}");
             }
+        }
+        
+        private static bool isFightingBoss()
+        {
+            var getCurrentMusic = typeof(MusicMan).GetMethod("GetCurrentMusic", BindingFlags.NonPublic | BindingFlags.Instance);
+            string currentMusic = getCurrentMusic != null ? (string)getCurrentMusic.Invoke(MusicMan.instance, new object[] {} ) : "";
+            if (currentMusic.Contains("boss"))
+            {
+                Logger.LogInfo("Skipping message, you're in an important fight!");
+                return true;
+            }
+
+            return false;
         }
     }
 }
