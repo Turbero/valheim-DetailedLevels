f9f516b2b09274d660f3bd6d3e37c0576e27e05a
Turbero <dicon18@gmail.com>
2025-10-03 16:55:50 +0200

    Skipping skill up message when fighting hildir bosses

--- DetailedLevels/Features/PlayerSkillupMessage.cs
+++ DetailedLevels/Features/PlayerSkillupMessage.cs
@@ -1,17 +1,30 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using UnityEngine;
 
 namespace DetailedLevels.Features
 {
     [HarmonyPatch(typeof(Player), nameof(Player.OnSkillLevelup))]
     class Player_Skillup_Patch
     {
+        private static readonly List<String> hildirBossesKeys = new List<string>
+        {
+            "$enemy_skeletonfire",
+            "$enemy_fenringcultist_hildir",
+            "$enemy_goblinbrute_hildircombined",
+            "$enemy_goblinbrute_hildir",
+            "$enemy_goblin_hildir"
+        };
         static void Postfix(ref Skills.SkillType skill, ref float level)
         {
+            if (isFightingBoss()) return;
+            
             //Check big message first
             int multipleBigValue = Math.Max(0, Math.Min(100, ConfigurationFile.skillUpBigMessageAfterMultipleLevel.Value));
-            if (multipleBigValue > 0 && ((int)level % multipleBigValue == 0 || (int)level == 100) && !isFightingBoss())
+            if (multipleBigValue > 0 && ((int)level % multipleBigValue == 0 || (int)level == 100))
             {
                 MessageHud.instance.ShowBiomeFoundMsg($"$skill_{skill.ToString().ToLower()}: {(int)level}", true);
                 return;
@@ -19,7 +32,7 @@ namespace DetailedLevels.Features
 
             //Check yellow message
             int multipleValue = Math.Max(0, Math.Min(100, ConfigurationFile.skillUpMessageAfterMultipleLevel.Value));
-            if (multipleValue > 0 && (int)level % multipleValue == 0 && !isFightingBoss())
+            if (multipleValue > 0 && (int)level % multipleValue == 0)
             {
                 MessageHud.instance.ShowMessage(MessageHud.MessageType.Center, $"$msg_skillup $skill_{skill.ToString().ToLower()}: {(int)level}");
             }
@@ -31,9 +44,24 @@ namespace DetailedLevels.Features
             string currentMusic = getCurrentMusic != null ? (string)getCurrentMusic.Invoke(MusicMan.instance, new object[] {} ) : "";
             if (currentMusic.Contains("boss"))
             {
-                Logger.LogInfo("Skipping message, you're in an important fight!");
+                Logger.LogInfo("Skipping skill up message, you're in an important world fight!");
                 return true;
             }
+            
+            //Hildir quest
+            var enemies = Character.GetAllCharacters().Where(c =>
+                c != null &&
+                !c.IsPlayer() &&
+                c.GetBaseAI().IsAlerted() &&
+                Vector3.Distance(c.transform.position, Player.m_localPlayer.transform.position) < 25f
+            );
+
+            foreach (var enemy in enemies)
+                if (hildirBossesKeys.Contains(enemy.m_name))
+                {
+                    Logger.LogInfo("Skipping skill up message, you're in an important Hildir fight!");
+                    return true;
+                }
 
             return false;
         }
